132f5a6b7a100a93811f43ea3b0d6eb8b6716fde
adamlahbib <adem.lahbib@insat.ucar.tn>
2024-11-15 23:24:18 +0100

    Add monitoring components using Helm
    This commit adds new monitoring components to the project using Helm. The following components were added:
    - Falco: A container runtime security tool
    - Falco Sidekick: A companion tool for Falco
    - Crowdsec: A security automation tool
    
    These components are deployed in the "monitoring" namespace and are configured with specific versions and settings.

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -41,3 +41,47 @@ resource "helm_release" "prometheus" {
     namespace  = "monitoring"
     version    = "15.10.1"
 }
+
+resource "helm_release" "falco" {
+    name       = "falco"
+    repository = "https://falcosecurity.github.io/charts"
+    chart      = "falco"
+    namespace  = "monitoring"
+    version    = "4.14.1"
+
+    set {
+        name  = "ebpf.enabled"
+        value = "true"
+    }
+}
+
+resource "helm_release" "falco_sidekick" {
+    name       = "falco-sidekick"
+    repository = "https://falcosecurity.github.io/charts"
+    chart      = "falco-sidekick"
+    namespace  = "monitoring"
+    version    = "0.8.9"
+
+    set {
+        name  = "falcosidekick.enabled"
+        value = "true"
+    }
+
+    set {
+        name  = "falco.webui.enabled"
+        value = "true"
+    }
+
+    set {
+        name  = "falco.webui.service.type"
+        value = "NodePort"
+    }
+}
+
+resource "helm_release" "crowdsec" {
+    name       = "crowdsec"
+    repository = "https://crowdsecurity.github.io/helm-charts"
+    chart      = "crowdsec"
+    namespace  = "monitoring"
+    version    = "0.13.0"
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,15 +11,18 @@ terraform {
         helm = {
             source  = "hashicorp/helm"
             version = ">= 2.0.2"
-            kubernetes {
-                host = aws_eks_cluster.eks_cluster.endpoint
-                token = aws_eks_cluster_auth.cluster_auth.token
-                insecure = true
-            }
         }
     }
 }
 
 provider "aws" {
     region = var.aws_region
+}
+
+provider "helm" {
+    kubernetes {
+        host = aws_eks_cluster.eks_cluster.endpoint
+        token = aws_eks_cluster_auth.cluster_auth.token
+        insecure = true
+    }
 }
