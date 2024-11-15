80b6e96bf22c8a81707f49ba0e8d0ed2e248936c
adamlahbib <adem.lahbib@insat.ucar.tn>
2024-11-15 22:25:53 +0100

    Update terraform main.tf to configure backend for S3 storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,10 @@
+terraform {
+  backend "s3" {}
+}
+
 resource "aws_eks_cluster" "eks_cluster" {
     name     = var.cluster_name
-    role_arn = aws_iam_role.eks_cluster_role.arn
+    role_arn = aws_iam_role.eks_role.arn
 
     vpc_config {
         subnet_ids = module.vpc.private_subnets
