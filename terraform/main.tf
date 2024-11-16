ee2086363732c78c8507eb638d7c0adbbff3b0e2
adamlahbib <adem.lahbib@insat.ucar.tn>
2024-11-16 03:36:44 +0100

    Refactor main.tf: Remove vpc_zone_identifier from autoscaling group configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,8 +112,6 @@ resource "aws_autoscaling_group" "eks_nodes" {
     max_size     = 3
     min_size     = 1
 
-    vpc_zone_identifier = [var.aws_zone]
-
     launch_template {
         id = aws_launch_template.eks_launch_template.id
         version = "$Latest"
