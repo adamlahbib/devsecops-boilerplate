923406af76aeb7afab633f23686efcde25864bd9
adamlahbib <adem.lahbib@insat.ucar.tn>
2024-11-16 03:13:29 +0100

    Refactor main.tf: Update scaling configuration for EKS node group

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,15 +106,18 @@ resource "aws_eks_node_group" "eks_node_group" {
     node_group_name = "eks-node-group"
     node_role_arn   = aws_iam_role.worker_role.arn
     subnet_ids      = module.vpc.private_subnets
-    desired_size = 2
-    min_size     = 1
-    max_size     = 3
 
     vpc_zone_identifier = module.vpc.subnet_zones[0]
 
     launch_template {
         id = aws_launch_template.eks_launch_template.id
         version = "$Latest"
+    }
+
+    scaling_config {
+        desired_size = 2
+        max_size     = 3
+        min_size     = 1
     }
 
     tag {
