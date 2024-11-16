5a2a17390b074945d8c067c17072ac3a7c34d3d7
adamlahbib <adem.lahbib@insat.ucar.tn>
2024-11-16 17:45:28 +0100

    Refactor Cloudflare credentials and append token to Terraform variables

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,16 +1,17 @@
 resource "cloudflare_record" "app" {
     zone_id = var.CLOUDFLARE_ZONE_ID
     name    = var.dns_name
-    value   = try(
+    content   = try(
         kubernetes_ingress_v1.prod-ingress.status[0].load_balancer[0].ingress[0].hostname,
         "PENDING_LB_HOSTNAME"
     )
     type    = "CNAME"
     proxied = true
+    allow_overwrite = true
 
     lifecycle {
         ignore_changes = [
-            value
+            content
         ]
     }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,7 +14,7 @@ terraform {
         }
         cloudflare = {
             source  = "cloudflare/cloudflare"
-            version = "= 2.12.0"
+            version = "~> 5.0"
         }
     }
 }
