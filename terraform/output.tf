b04f7fb80fa0e0266c78f99cfe978eaaaf1b8183
adamlahbib <adem.lahbib@insat.ucar.tn>
2024-11-16 04:21:54 +0100

    Refactor ingress.tf, monitoring.tf, outputs.tf, and security.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,13 +1,13 @@
 output "region" {
-    value = var.aws_region
+    value = aws_eks_cluster.eks_cluster.region
 }
 
 output "cluster_name" {
-    value = var.cluster_name
+    value = aws_eks_cluster.eks_cluster.name
 }
 
 output "repository_name" {
-    value = var.repository_name
+    value = aws_ecr_repository.ecr_repo.name
 }
 
 output "cluster_ip" {
