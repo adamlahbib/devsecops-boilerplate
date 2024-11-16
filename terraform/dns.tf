677a8e83981870328a56017d0d532e39c32a9b1a
adamlahbib <adem.lahbib@insat.ucar.tn>
2024-11-16 20:15:41 +0100

    Refactor DNS configuration to use updated Kubernetes service for hostname retrieval

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,7 +2,7 @@ resource "cloudflare_record" "app" {
     zone_id = var.CLOUDFLARE_ZONE_ID
     name    = var.dns_name
     content   = try(
-        data.kubernetes_service.nginx_ingress.status[0].load_balancer.ingress[0].hostname,
+        data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname,
         "PENDING_LB_HOSTNAME"
     )
     type    = "CNAME"
