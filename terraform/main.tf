a12ca234fa44166d191ee83f2e1327e6e3cda624
adamlahbib <adem.lahbib@insat.ucar.tn>
2024-11-16 04:00:46 +0100

    Refactor main.tf: Update EKS node subnet_ids configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,7 +102,7 @@ resource "aws_eks_node_group" "eks_nodes" {
     cluster_name    = aws_eks_cluster.eks_cluster.name
     node_group_name = "eks-node-group"
     node_role_arn   = aws_iam_role.worker_role.arn
-    subnet_ids      = [module.vpc.private_subnets]
+    subnet_ids      = module.vpc.private_subnets
     ami_type        = "AL2_x86_64"
     instance_types  = ["t3.micro"]
     disk_size       = 20
