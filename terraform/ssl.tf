8510287fe9c1054cd20fab4d2e00083bf583179b
adamlahbib <adem.lahbib@insat.ucar.tn>
2024-11-17 01:24:50 +0100

    Refactor SSL configuration and add cert-manager namespace and cluster issuer

--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -1,7 +1,18 @@
+resource "kubernetes_namespace" "cert_manager" {
+    metadata {
+        name = "cert-manager"
+        labels = {
+            "name" = "cert-manager"
+        }
+    }
+}
+
 resource "helm_release" "cert_manager" {
     name       = "cert-manager"
     repository = "https://charts.jetstack.io"
     chart      = "cert-manager"
+    version    = "v1.12.3"
+    namespace  = kubernetes_namespace.cert_manager.metadata.0.name
 
     set {
         name  = "installCRDs"
@@ -9,32 +20,32 @@ resource "helm_release" "cert_manager" {
     }
 }
 
-resource "kubernetes_namespace" "cert_manager" {
-    metadata {
-        name = "cert-manager"
-    }
-}
 
-resource "kubernetes_certificate" "cluster_issuer" {
-    metadata {
-        name      = "letsencrypt-prod"
-        namespace = "cert-manager"
-    }
-    spec {
-    acme {
-            email      = var.CLOUDFLARE_EMAIL
-            server     = "https://acme-v02.api.letsencrypt.org/directory"
-            privateKeySecretRef {
-            name = "letsencrypt-prod-key"
+resource "kubernetes_manifest" "cluster_issuer" {
+    manifest = {
+        apiVersion = "cert-manager.io/v1"
+        kind       = "ClusterIssuer"
+
+        metadata {
+            name      = "letsencrypt-prod"
+            namespace = "cert-manager"
         }
-        solvers {
-            dns01 {
-                cloudflare {
-                    email    = var.CLOUDFLARE_EMAIL
-                    apiToken = var.CLOUDFLARE_TOKEN
+        spec {
+            acme {
+                    email      = var.CLOUDFLARE_EMAIL
+                    server     = "https://acme-v02.api.letsencrypt.org/directory"
+                    privateKeySecretRef {
+                    name = "letsencrypt-prod-key"
+                }
+                solvers {
+                    dns01 {
+                        cloudflare {
+                            email    = var.CLOUDFLARE_EMAIL
+                            apiToken = var.CLOUDFLARE_TOKEN
+                        }
+                    }
                 }
             }
         }
-    }
     }
 }
